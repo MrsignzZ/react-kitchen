393f1a87539fdec2592ef04e930adabdfb8f7bc1
1440806960@qq.come <1440806960@qq.com>
2019-03-02 00:31:37 +0800

    页面路由

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import  Admin from "./admin";
+import Home from './pages/Home'
+import Detail from './pages/Detail'
+import NoMatch from './pages/NoMatch'
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Admin>
+          <Switch>
+            <Route path="/common" render={() => (
+              <Route path="/common/detail/:id" component={Detail} />
+            )} />
+            <Route path="/" render={() => (
+              <Switch>
+                <Route path="/home" component={Home} />
+                {/* <Redirect to="/home" /> */}
+                <Route component={NoMatch} />
+              </Switch>
+            )} />
+          </Switch>
+        </Admin>
+      </Router>
+    );
+  }
+}
+
+export default App;
