c316ecf60d6fcf499e1e7e8efb8b1668dfe848ad
1440806960@qq.come <1440806960@qq.com>
2019-03-02 00:36:50 +0800

    Footer组件

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.less'
+export default class Footer extends React.Component{
+
+  render() {
+    return (
+      <div className="footer">
+        版权所有: 小榕同学(推荐使用谷歌浏览器, 可以获得更佳操作体验)
+      </div>
+    )
+  }
+}
