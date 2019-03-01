8d76e1116140013686dbb1bd42b324dedd006e10
1440806960@qq.come <1440806960@qq.com>
2019-03-02 00:37:51 +0800

    NavLeft组件

---
+++ src/components/NavLeft/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Menu} from 'antd';
+import { NavLink } from 'react-router-dom'
+import MenuConfig from '../../config/menuConfig'
+import './index.less';
+
+const SubMenu = Menu.SubMenu;
+export default class NavLeft extends React.Component {
+
+  componentWillMount() {
+    const menuTreeNode = this.renderMenu(MenuConfig);
+    this.setState({
+      menuTreeNode
+    })
+  }
+  // // 菜单渲染
+  renderMenu = (data) => {
+    return data.map((item) => {
+      if (item.children) {
+        return (
+          <SubMenu title={item.title} key={item.key}>
+            {this.renderMenu(item.children)}
+          </SubMenu>
+        )
+      }
+      return <Menu.Item title={item.title} key={item.key}>
+        <NavLink to={item.key}>{item.title}</NavLink>
+        {/* {item.title} */}
+      </Menu.Item>
+    })
+  }
+  render() {
+    return (
+      <div>
+        <Menu
+          onClick={this.handleClick}
+        >
+          {this.state.menuTreeNode}
+        </Menu>
+      </div>
+    )
+  }
+}
