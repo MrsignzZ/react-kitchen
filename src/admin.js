94bdf157ce96c620d6978b137a5d0b0eebeeaea9
1440806960@qq.come <1440806960@qq.com>
2019-03-02 00:33:31 +0800

    首页

---
+++ src/admin.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+import Header from './components/Header';
+import NavLeft from './components/NavLeft';
+import Footer from './components/Footer'
+import './common.less';
+import NavRight from './components/NavRight';
+export default class Admin extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <Header />
+        <div className="content">
+          <Row type="flex" justify="center">
+            <Col span="2">
+              <NavLeft />
+            </Col>
+            <Col span="10">
+              {/* <Home /> */}
+              {this.props.children}
+            </Col>
+            <Col span="5" >
+              <NavRight />
+            </Col>
+          </Row>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
