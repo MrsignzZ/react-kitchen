12487a99fd30498e3e4a6566c3a1814e36955db0
1440806960@qq.come <1440806960@qq.com>
2019-03-02 16:53:40 +0800

    封装了了获取数组随机元素方法

--- src/utils/index.js
+++ src/utils/index.js
@@ -13,5 +13,16 @@ export default {
         type: index.type === '0' ? '配料' : '主材'
       }
     })
+  },
+  // 随机获取数组元素
+  getRandomArrayElements(arr, count) {
+    var shuffled = arr.slice(0), i = arr.length, min = i - count, temp, index;
+    while (i-- > min) {
+      index = Math.floor((i + 1) * Math.random());
+      temp = shuffled[index];
+      shuffled[index] = shuffled[i];
+      shuffled[i] = temp;
+    }
+    return shuffled.slice(min);
   }
 }
