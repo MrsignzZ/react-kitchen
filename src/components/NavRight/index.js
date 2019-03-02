684b8994d427bbe011a7e5bda046b65087863820
1440806960@qq.come <1440806960@qq.com>
2019-03-02 16:54:56 +0800

    动态生成热门标签

--- src/components/NavRight/index.js
+++ src/components/NavRight/index.js
@@ -1,8 +1,57 @@
 import React from 'react';
 import { Tag, Divider } from 'antd';
+import { NavLink } from 'react-router-dom'
 import './index.less'
-
+import Axios from '../../api'
+import Utils from '../../utils'
 export default class NavRight extends React.Component {
+
+  state = {
+    tagList: []
+  }
+
+  componentWillMount(){
+    this.getTagAPIList()
+  }
+  // 渲染标签列表
+  renderTagList = (data) => {
+    return data.map((item) => {
+      // console.log(item.list.slice(0, 10));
+      return (
+        <li className="tags-content">
+          <h4>{item.name}</h4>
+          <div>
+            {Utils.getRandomArrayElements(item.list, 8).map((tag) => (
+                <Tag color="magenta">{tag.name}</Tag>
+            ))}
+          </div>
+        </li>
+      )
+    })
+  }
+
+  // 从接口获取标签列表数据
+  getTagAPIList = () => {
+    // let classid = 2 // 标签序号
+    // let start = 0   // 其实条数
+    // let num = 12    // 获取数量
+
+    Axios
+      .jsonp({
+        url: 'http://api.jisuapi.com/recipe/class?appkey=9d1f6ec2fd2463f7'
+      })
+      .then((res) => {
+        if (res.status === '0') {
+          console.log(res.result[0]);
+          let tagList = this.renderTagList(res.result)
+          // console.log(tagList);
+          this.setState({
+            tagList: tagList
+          })
+        }
+      })
+  }
+
   render() {
     return (
       <div className="aside">
@@ -11,70 +60,7 @@ export default class NavRight extends React.Component {
           <span className="link-more"><a href="">更多>></a></span>
         </h3>
         <ul className="tags">
-          <li className="tags-content">
-            <h4>小吃</h4>
-            <div>
-              <Tag color="magenta">magenta</Tag>
-              <Tag color="red">red</Tag>
-              <Tag color="volcano">volcano</Tag>
-              <Tag color="orange">orange</Tag>
-              <Tag color="gold">gold</Tag>
-              <Tag color="lime">lime</Tag>
-              <Tag color="green">green</Tag>
-              <Tag color="cyan">cyan</Tag>
-              <Tag color="blue">blue</Tag>
-              <Tag color="geekblue">geekblue</Tag>
-              <Tag color="purple">purple</Tag>
-            </div>
-          </li>
-          <li>
-            <h4>小吃</h4>
-            <div>
-              <Tag color="magenta">magenta</Tag>
-              <Tag color="red">red</Tag>
-              <Tag color="volcano">volcano</Tag>
-              <Tag color="orange">orange</Tag>
-              <Tag color="gold">gold</Tag>
-              <Tag color="lime">lime</Tag>
-              <Tag color="green">green</Tag>
-              <Tag color="cyan">cyan</Tag>
-              <Tag color="blue">blue</Tag>
-              <Tag color="geekblue">geekblue</Tag>
-              <Tag color="purple">purple</Tag>
-            </div>
-          </li>
-          <li>
-            <h4>小吃</h4>
-            <div>
-              <Tag color="magenta">magenta</Tag>
-              <Tag color="red">red</Tag>
-              <Tag color="volcano">volcano</Tag>
-              <Tag color="orange">orange</Tag>
-              <Tag color="gold">gold</Tag>
-              <Tag color="lime">lime</Tag>
-              <Tag color="green">green</Tag>
-              <Tag color="cyan">cyan</Tag>
-              <Tag color="blue">blue</Tag>
-              <Tag color="geekblue">geekblue</Tag>
-              <Tag color="purple">purple</Tag>
-            </div>
-          </li>
-          <li>
-            <h4>小吃</h4>
-            <div>
-              <Tag color="magenta">magenta</Tag>
-              <Tag color="red">red</Tag>
-              <Tag color="volcano">volcano</Tag>
-              <Tag color="orange">orange</Tag>
-              <Tag color="gold">gold</Tag>
-              <Tag color="lime">lime</Tag>
-              <Tag color="green">green</Tag>
-              <Tag color="cyan">cyan</Tag>
-              <Tag color="blue">blue</Tag>
-              <Tag color="geekblue">geekblue</Tag>
-              <Tag color="purple">purple</Tag>
-            </div>
-          </li>
+          {this.state.tagList}
         </ul>
       </div>
     );
